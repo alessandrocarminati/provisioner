9c1654d52ae2707f68b3d2082b49cf344cb61a7e
Alessandro Carminati <alessandro.carminati@gmail.com>
2024-03-18 14:53:33 +0100

    add tasmota fence device
    This commit tackle #2 partially .
    
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- commands.go
+++ commands.go
@@ -145,27 +145,87 @@ func dummyCmd(input string) string{
 func ton(input string) string{
 	res:="Error config problem"
 	configOK := true
-	oid, ok := monitorConfig["snmp_pdu_ctrl_oid"]
-	configOK = configOK && ok
-	host, ok := monitorConfig["snmp_pdu_ctrl_host"]
-	configOK = configOK && ok
-	user, ok := monitorConfig["snmp_pdu_ctrl_user"]
-	configOK = configOK && ok
-	onValue, ok := monitorConfig["snmp_pdu_ctrl_on_val"]
-	configOK = configOK && ok
-	val, err := strconv.Atoi(onValue)
-	if err != nil {
-		configOK=false
-	}
-	if configOK {
-		res="on command sent"
-		err := snmpSetv3unsec(oid, val, host, user)
-		if err != nil {
-			res=fmt.Sprintf("Error setting SNMP: %s", err.Error())
+	pdu_type, ok := monitorConfig["pdu_type"]
+	if ok {
+		switch pdu_type {
+		case "tasmota":
+			tasmota_host, ok := monitorConfig["tasmota_host"]
+			if ok {
+				res="on command sent"
+				err := TasmotaSetState(tasmota_host, "ON")
+				if err != nil {
+					res=fmt.Sprintf("Error setting Tasmota device: %s", err.Error())
+				}
+			}
+		case "snmp":
+			oid, ok := monitorConfig["snmp_pdu_ctrl_oid"]
+			configOK = configOK && ok
+			host, ok := monitorConfig["snmp_pdu_ctrl_host"]
+			configOK = configOK && ok
+			user, ok := monitorConfig["snmp_pdu_ctrl_user"]
+			configOK = configOK && ok
+			onValue, ok := monitorConfig["snmp_pdu_ctrl_on_val"]
+			configOK = configOK && ok
+			val, err := strconv.Atoi(onValue)
+			if err != nil {
+				configOK=false
+			}
+			if configOK {
+				res="on command sent"
+				err := snmpSetv3unsec(oid, val, host, user)
+				if err != nil {
+					res=fmt.Sprintf("Error setting SNMP: %s", err.Error())
+				}
+			}
+		default:
+			res="unknown PDU type"
 		}
 	}
 	return res + "\r\n"
 }
+
+func toff(input string) string{
+	res:="Error config problem"
+	configOK := true
+	pdu_type, ok := monitorConfig["pdu_type"]
+	if ok {
+		switch pdu_type {
+		case "tasmota":
+			tasmota_host, ok := monitorConfig["tasmota_host"]
+			if ok {
+				res="off command sent"
+				err := TasmotaSetState(tasmota_host, "OFF")
+				if err != nil {
+					res=fmt.Sprintf("Error setting Tasmota device: %s", err.Error())
+				}
+			}
+		case "snmp":
+			oid, ok := monitorConfig["snmp_pdu_ctrl_oid"]
+			configOK = configOK && ok
+			host, ok := monitorConfig["snmp_pdu_ctrl_host"]
+			configOK = configOK && ok
+			user, ok := monitorConfig["snmp_pdu_ctrl_user"]
+			configOK = configOK && ok
+			offValue, ok := monitorConfig["snmp_pdu_ctrl_on_val"]
+			configOK = configOK && ok
+			val, err := strconv.Atoi(offValue)
+			if err != nil {
+				configOK=false
+			}
+			if configOK {
+				res="off command sent"
+				err := snmpSetv3unsec(oid, val, host, user)
+				if err != nil {
+					res=fmt.Sprintf("Error setting SNMP: %s", err.Error())
+				}
+			}
+		default:
+			res="unknown PDU type"
+		}
+	}
+	return res + "\r\n"
+}
+/*
 func toff(input string) string{
 	res:="Error config problem"
 	configOK := true
@@ -190,7 +250,7 @@ func toff(input string) string{
 	}
 	return res + "\r\n"
 }
-
+*/
 func echoCmd(input string) string{
 
 	log.Printf("echoCmd arg'%s'\n", input)
---
+++ tasmota.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type DeviceState struct {
+	Power string `json:"POWER"`
+}
+
+func TasmotaQueryState(host string) (bool, error) {
+	url := fmt.Sprintf("http://%s/cm?cmnd=power", host)
+	resp, err := http.Get(url)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, errors.New("unable to get device state")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	var deviceState DeviceState
+	err = json.Unmarshal(body, &deviceState)
+	if err != nil {
+		return false, err
+	}
+
+	switch deviceState.Power {
+	case "ON":
+		return true, nil
+	case "OFF":
+		return false, nil
+	default:
+		return false, errors.New("unknown device state")
+	}
+}
+
+func TasmotaSetState(host string, state string) error {
+	if state != "ON" && state != "OFF" {
+		return errors.New("invalid state provided")
+	}
+
+	url := fmt.Sprintf("http://%s/cm?cmnd=power+%s", host, state)
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("unable to set device state")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var deviceState DeviceState
+	err = json.Unmarshal(body, &deviceState)
+	if err != nil {
+		return err
+	}
+
+	if deviceState.Power != state {
+		return errors.New("device state not set correctly")
+	}
+
+	return nil
+}
+
