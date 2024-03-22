02cade225c955197dc78aea58b308644a596a105
Alessandro Carminati <alessandro.carminati@gmail.com>
2024-03-22 18:04:41 +0100

    removed debug from min.sh script
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- min.sh
+++ min.sh
@@ -5,7 +5,6 @@ else
 	first_tag=$(git show-ref --tags | tail -n1| cut -d" " -f1)
 	last_commit=$(git log --pretty=oneline | head -n1| cut -d" " -f1)
 	num_commits=$(git rev-list --count $first_tag..$last_commit)
-	echo "first_tag=$first_tag, last_commit=$last_commit, num_commits=$num_commits"
 fi
 echo "${num_commits:-0}"
 
