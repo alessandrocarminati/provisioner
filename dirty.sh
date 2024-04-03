653c026cc01be2625f7df2bcf8cf15b6d847e554
Alessandro Carminati <alessandro.carminati@gmail.com>
2024-04-03 22:36:20 +0200

    make: dirti hashing
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- dirty.sh
+++ dirty.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 state="clean"
-git status --untracked-files=no --porcelain|  grep -q M && state="dirty"
+git status --untracked-files=no --porcelain|  grep -q M && hash=$(git diff | md5sum | cut -d ' ' -f1) && state="dirty-${hash:0:5}"
 echo $state
